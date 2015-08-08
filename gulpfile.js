e5be25dcc5bf292af2ac884fd0a692c8577f8bdc
FladeX <FladeX@yandex.ru>
2015-08-08 15:45:48 +0300

    add gulp

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+var gulp = require('gulp');
+var stylus = require('gulp-stylus');
+var concat = require('gulp-concat');
+
+gulp.task('stylus', function() {
+  gulp.src('source/stylus/*.styl')
+    .pipe(stylus())
+    .pipe(gulp.dest('source/css'));
+});
+
+gulp.task('concat-css', function() {
+  gulp.src(['source/css/reset.css', 'source/css/main.css'])
+    .pipe(concat('style.css'))
+    .pipe(gulp.dest('assets/css'));
+});
+
+gulp.task('default', ['stylus'], function() {
+  gulp.watch('source/stylus/*.styl', ['stylus']);
+});
