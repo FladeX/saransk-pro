ed5874ef861d5eabf90d9a1e6e2951cbde6dba9f
FladeX <FladeX@yandex.ru>
2015-08-30 11:36:33 +0300

    fix scroll position when popup is opened

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,48 @@
 $(document).ready(function() {
+  // left: 37, up: 38, right: 39, down: 40,
+  // spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
+  var keys = {
+    37: 1,
+    38: 1,
+    39: 1,
+    40: 1
+  };
+
+  function preventDefault(e) {
+    e = e || window.event;
+    if (e.preventDefault) {
+      e.preventDefault();
+    }
+    e.returnValue = false;
+  }
+
+  function preventDefaultForScrollKeys(e) {
+    if (keys[e.keyCode]) {
+      preventDefault(e);
+      return false;
+    }
+  }
+
+  function disableScroll() {
+    if (window.addEventListener) { // older FF
+      window.addEventListener('DOMMouseScroll', preventDefault, false);
+    }
+    window.onwheel = preventDefault; // modern standard
+    window.onmousewheel = document.onmousewheel = preventDefault; // older browsers, IE
+    window.ontouchmove = preventDefault; // mobile
+    document.onkeydown = preventDefaultForScrollKeys;
+  }
+
+  function enableScroll() {
+    if (window.removeEventListener) {
+      window.removeEventListener('DOMMouseScroll', preventDefault, false);
+    }
+    window.onmousewheel = document.onmousewheel = null;
+    window.onwheel = null;
+    window.ontouchmove = null;
+    document.onkeydown = null;
+  }
+
   $('#order').on('click', function() {
     $('#order_popup').show();
     $('#order_popup').css({
@@ -13,17 +57,13 @@ $(document).ready(function() {
       'top': '50%',
       'margin-top': '-100px'
     });
-    $('.b-page').css({
-      'overflow': 'hidden'
-    });
+    disableScroll();
   });
   $('#order_popup').on('click', function(e) {
     if (e.target != this) {
       return;
     }
     $('#order_popup').hide();
-    $('.b-page').css({
-      'overflow': 'auto'
-    });
+    enableScroll();
   });
 });
